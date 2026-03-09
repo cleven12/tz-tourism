afdf6861f247b1e746c95d28ea609254260d3758
cleven12 <168540659+cleven12@users.noreply.github.com>
2026-03-09 15:59:26 +0300

    fix: set rootDir=src in render.yaml, simplify all deployment commands

--- src/build.sh
+++ src/build.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
+pip install -r ../requirements.txt
 
-export DJANGO_SETTINGS_MODULE=cofig.settings
-export PYTHONPATH="$PYTHONPATH:$(pwd)/src"
-
-python src/manage.py collectstatic --no-input
-python src/manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
